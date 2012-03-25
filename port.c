85e150b233e6cbd616393c1e0a0b1367e9f8d292
Yuriy <yuriy.kulikov.87@googlemail.com>
2012-03-25 22:45:19 +0200

    Added licenses
    	modified:   port.c
    	modified:   portmacro.h

--- port.c
+++ port.c
@@ -1,32 +1,22 @@
 /* This file has been prepared for Doxygen automatic documentation generation.*/
-/*! \file *********************************************************************
- *
- * \brief  XMEGA port source for FreeRTOS 
- *
- *      This file contains definitions and routines requred for FreeRTOS.
- *		This port uses XMega Real Time Clock to provide constand task switching
- *		frequency, regardless of main COU frequency changes.
- *		If RTC is used in application, use any other GP timer for RTOS needs.
- *		This file is based on port.c for ATmega323,
- *		Copyright (C) 2010 Real Time Engineers Ltd.
- *		Original file was modified to suit XMega. Changed counter interrupt
- *		source. Routines portSAVE_CONTEXT(), portRESTORE_CONTEXT()
- *		and *pxPortInitialiseStack() were modified to work in extended progam
- *		memory space.
- *		
- * \par Documentation
- *		http://www.FreeRTOS.org - Documentation, latest information, license and
- *		contact details.
- *		
- * \author
- *      Universität Erlangen-Nürnberg
- *		LS Informationstechnik (Kommunikationselektronik)
- *		Yuriy Kulikov
+/*
+ * Copyright (C) 2012 Yuriy Kulikov
+ *      Universitaet Erlangen-Nuernberg
+ *      LS Informationstechnik (Kommunikationselektronik)
  *      Support email: Yuriy.Kulikov.87@googlemail.com
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *****************************************************************************/
-
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /* Compiler definitions include file. */
 #include "avr_compiler.h"
